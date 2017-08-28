3d5eda67635f6b2361179b5dd82ec965d222be5d
zt515 <kiva515@foxmail.com>
2017-08-28 18:24:41 +0800

    Project: Register source files in CMake

--- include/core/dvm.hpp
+++ include/core/dvm.hpp
@@ -2,6 +2,8 @@
 
 #include <core/config.hpp>
 #include <core/type.hpp>
+#include <core/exceptions.hpp>
+#include <core/errorcodes.hpp>
 #include <core/interpreter.hpp>
 
 extern "C" int vm_main(int argc, const char **argv);
--- include/core/exceptions.hpp
+++ include/core/exceptions.hpp
@@ -2,20 +2,29 @@
 // DVM Exceptions
 #include <exception>
 #include <string>
-namespace dvm::core {
-    class exception final:public std::exception {
-		std::string mWhat;
-	public:
-		exception()=delete;
-        exception(const std::string& what):mWhat(what) {}
-        exception(const exception&)=default;
-		exception(exception&&)=default;
-		virtual ~exception()=default;
-		exception& operator=(const exception&)=default;
-		exception& operator=(exception&&)=default;
-		virtual const char* what() const noexcept override
-		{
-			return this->mWhat.c_str();
-		}
-	};
+
+namespace dvm {
+    namespace core {
+        class exception final : public std::exception {
+            std::string mWhat;
+        public:
+            exception() = delete;
+
+            exception(const std::string &what) : mWhat(what) { }
+
+            exception(const exception &) = default;
+
+            exception(exception &&) = default;
+
+            virtual ~exception() = default;
+
+            exception &operator=(const exception &)= default;
+
+            exception &operator=(exception &&)= default;
+
+            virtual const char *what() const noexcept override {
+                return this->mWhat.c_str();
+            }
+        };
+    }
 }
