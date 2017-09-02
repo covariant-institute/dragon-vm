469dbe92eb2c87e266d6b24a2c09ddd831a994e9
zt515 <kiva515@foxmail.com>
2017-09-02 22:53:23 +0800

    Object: more basic objects

--- src/core/object/basic_classes.cpp
+++ src/core/object/basic_classes.cpp
@@ -8,7 +8,19 @@ namespace dvm {
     namespace core {
         namespace object {
             void create_basic_classes() {
-                Class::define_bootstrap_class("Object", 1);
+                Class *object = Class::define_bootstrap_class("Object", 1);
+                Class::define_class(object, "Int8", 2);
+                Class::define_class(object, "Int16", 2);
+                Class::define_class(object, "Int32", 2);
+                Class::define_class(object, "Int64", 2);
+                Class::define_class(object, "UInt8", 2);
+                Class::define_class(object, "UInt16", 2);
+                Class::define_class(object, "UInt32", 2);
+                Class::define_class(object, "UInt64", 2);
+                Class::define_class(object, "Bool", 2);
+                Class::define_class(object, "Byte", 2);
+                Class::define_class(object, "Float", 2);
+                Class::define_class(object, "Double", 2);
             }
         }
     }
