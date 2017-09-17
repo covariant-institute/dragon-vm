e3f350b32e3997be4f02d9f01b5bf798ff09f5f8
Michael Lee <mikecovlee@163.com>
2017-09-17 19:35:27 +0800

    支持格式化异常信息

--- include/core/exceptions.hpp
+++ include/core/exceptions.hpp
@@ -2,29 +2,36 @@
 
 // DVM Exceptions
 #include <exception>
+#include <cstdio>
 #include <string>
 #include <utility>
 
 namespace dvm {
 	namespace core {
-		class exception final : public std::exception {
+		template<typename...args_t>std::string sprintf(const std::string& format,args_t&&...args) {
+			static char buffer[2048];
+			::snprintf(buffer,2048,format.c_str(),std::forward<args_t>(args)...);
+			return std::string(buffer);
+		}
+		class exception : public std::exception {
+		protected:
 			std::string mWhat;
 		public:
 			exception() = delete;
 
-			explicit exception(std::string what) : mWhat(std::move(what)) {}
+			template<typename...args_t>explicit exception(const std::string& what,args_t&&...args) : mWhat(dvm::core::sprintf(what,std::forward<args_t>(args)...)) {}
 
 			exception(const exception &) = default;
 
-			exception(exception &&) = default;
+			exception(exception &&) noexcept = default;
 
-			~exception() override = default;
+			virtual ~exception() = default;
 
 			exception &operator=(const exception &)= default;
 
 			exception &operator=(exception &&)= default;
 
-			const char *what() const noexcept override
+			virtual const char *what() const noexcept override
 			{
 				return this->mWhat.c_str();
 			}
