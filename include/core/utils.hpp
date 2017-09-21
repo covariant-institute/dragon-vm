069bfba91f0407443998fa866471d67d24765d86
zt515 <kiva515@foxmail.com>
2017-09-22 00:06:29 +0800

    Tests: Update register test

--- include/core/utils.hpp
+++ include/core/utils.hpp
@@ -2,3 +2,18 @@
 
 #define dvm_offset_of(TYPE, MEMBER) \
     ((size_t) &((TYPE *) nullptr)->MEMBER)
+
+namespace dvm {
+    namespace core {
+
+        template <typename T, typename R>
+        struct isSameType {
+            static constexpr bool value = false;
+        };
+
+        template <typename T>
+        struct isSameType<T, T> {
+            static constexpr bool value = true;
+        };
+    }
+}
--- tests/test-vm_register.cpp
+++ tests/test-vm_register.cpp
@@ -3,8 +3,9 @@
 //
 
 #include <core/runtime/vm_register.hpp>
-#include <cassert>
 #include <core/runtime/vm_context.hpp>
+#include <cassert>
+#include <core/object/slot.hpp>
 
 int main() {
     using namespace dvm::core::runtime::registers;
@@ -22,4 +23,7 @@ int main() {
     visitor->set(10086);
 
     assert(visitor->data.i32 == 10086);
+
+    visitor->set_unchecked(static_cast<dvm::core::UInt32>('Z'));
+    assert(visitor->data.i32 == 'Z');
 }
