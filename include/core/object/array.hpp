180b78ce2aeba4557794dda8206357d91a6e53be
zt515 <kiva515@foxmail.com>
2017-10-10 23:29:14 +0800

    Object: start Array

---
+++ include/core/object/array.hpp
@@ -0,0 +1,24 @@
+//
+// Created by kiva on 2017/10/10.
+//
+
+#pragma once
+
+#include <core/type.hpp>
+#include <core/object/slot.hpp>
+
+namespace dvm {
+    namespace core {
+        namespace object {
+            struct Array {
+                SizeT length;
+                TypeIdentifier array_type;
+                Slot elements[0];
+
+                static Array *new_array(SizeT length, TypeIdentifier array_type);
+
+                static Array *new_array(SizeT length);
+            };
+        }
+    }
+}
