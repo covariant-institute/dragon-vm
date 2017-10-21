59d1af3fa3ba1e2529222af4a041301afddafd2c
zt515 <kiva515@foxmail.com>
2017-10-21 20:06:51 +0800

    Core: remove unused imports

--- include/core/dvm.hpp
+++ include/core/dvm.hpp
@@ -4,6 +4,5 @@
 #include <core/type.hpp>
 #include <core/exceptions.hpp>
 #include <core/errorcodes.hpp>
-#include <core/runtime/interpreter.hpp>
 
 extern "C" int vm_main(int argc, const char **argv);
